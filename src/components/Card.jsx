e087b063fdc708ef0832db249baa215b5115a994
Luiz <luizmatheuslins5587@gmail.com>
2025-03-25 09:32:58 -0300

    Corrigido caminho das imagens e atualizado JSON

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,14 @@
 import { useEffect, useState } from "react";
-
-import placeholder from "../assets/placeholder.svg";
 import "./Card.css";
+
 const Card = () => {
   const [livros, setLivros] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:3000/livros")
       .then((res) => res.json())
       .then((data) => {
-        const livrosComImagem = data.map((livro) => ({
-          ...livro,
-          image: placeholder,
-        }));
-        setLivros(livrosComImagem);
+        setLivros(data);
       });
   }, []);
 
@@ -20,7 +16,7 @@ const Card = () => {
     <div className="card-container">
       {livros.map((livro) => (
         <div key={livro.id} className="card">
-          <img src={livro.image} alt={`Livro ${livro.id}`} />
+          <img src={livro.imagem} alt={`Livro ${livro.text}`} />
           <p>{livro.text}</p>
           <p>R$ {livro.value}</p>
         </div>
